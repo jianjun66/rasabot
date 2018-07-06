f2a2b861bc0df14fd380db88d8651bf14f31e8ab
Ubuntu <jxu@rasabotVM.vxzlcydshbne1a00suvwmhcxuf.bx.internal.cloudapp.net>
2018-07-06 18:36:37 +0000

    update the webhook

--- intelleibot/views.py
+++ intelleibot/views.py
@@ -8,17 +8,30 @@ from bot import train
 from bot import run_fb_webhook
 
 def webhook(request):
-    jsondata = request.body
-    print(jsondata)
-#    data = json.loads(jsondata, )
+    if request.method == 'GET':
+        # for fb webhook validation
+        hub_mode = request.GET["hub.mode"] # "sbscribe
+        hub_chanllenge = request.GET["hub.challenge"] # reutnr this value 
+        hub_verify_token = request.GET["hub.verify_token"] # this should match the value in fb_credential.yaml
+
+        if not hub_verify_tokne == 'intelleibot':
+         return HttpResponse("I don't recognize you", status=400)
+    elif request.method == 'POST':
     
-#    print(data["sender_id"])
-#    print(data["message"])
+        jsondata = json.loads(request.body)
+        print(jsondata)
     
-#    result = answer(data["message"], data["sender_id"])
-#    print("Bot says: {}".format(result))
-#    return HttpResponse(result, status=200)
-    return HttpResponse("Hi", status=200)
+#       data = json.loads(jsondata, )
+    
+#       print(data["sender_id"])
+#       print(data["message"])
+    
+#       result = answer(data["message"], data["sender_id"])
+#       print("Bot says: {}".format(result))
+#       return HttpResponse(result, status=200)
+        return HttpResponse('OK', status=200)
+    
+    return HttpResponse("Unknown method {}".format(request.method), status=500)
 
 def train_bot(request):
     train()
